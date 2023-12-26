0b1c189aa11b3640463a5d23838a090453a44d0c
Leftfish <bohdan@widla.net>
2023-12-26 20:27:56 +0100

    Add solution to Day 13 (both parts).

--- 13/d13.py
+++ 13/d13.py
@@ -1,58 +1,76 @@
-from math import ceil
-
 print('Day 13 of Advent of Code!')
 
 def parse_data(raw_data):
+    '''Parses input string into list of lists'''
+
     return [pattern.splitlines() for pattern in raw_data.split('\n\n')]
 
 
-def get_hashes(parsed_data):
-    '''Transforms lines and cols into hashes because I thought 
-    it was easier. Must use something else for part 2.'''
+def get_signatures(parsed_data):
+    '''Transforms lines and cols into lists of binary digits.'''
 
-    line_hashes = [hash(line) for line in parsed_data]
+    line_signatures = []
+    for line in parsed_data:
+        signature = [1 if char == '#' else 0 for char in line]
+        line_signatures.append(signature)
 
-    col_hashes = []
+    col_signatures = []
     for j in range(len(parsed_data[0])):
-        col = []
+        signature = []
         for i in range(len(parsed_data)):
-            col.append(parsed_data[i][j])
-        col_hashes.append(hash("".join(col)))
+            char = parsed_data[i][j]
+            signature.append(1 if char == '#' else 0)
+        col_signatures.append(signature)
+    return line_signatures, col_signatures
 
-    return line_hashes, col_hashes
+
+def compare_signatures(this, other):
+    '''Finds indices where signatures of lines or columns diverge'''
+    differences = []
+    for idx, pair in enumerate(zip(this, other)):
+        if pair[0] != pair[1]:
+            differences.append(idx)
+    return differences
 
 
-def find_reflection(data):
-    '''Find reflections by traversing columns and lines separately'''
+def find_reflections(signatures):
+    '''Starts from pairs of neighbouring lines/cols and goes outward
+    until the border. If sum of differing chars == 0: part 1 reflection.
+    If sum of differing chars == 1: part 2 reflection'''
+    results = [0 ,0]
+    starts = [(n, n+1) for n in range(len(signatures)-1)]
+    for idx, start in enumerate(starts):
+        i, j = start
+        sum_of_diffs = 0
+        while i >= 0 and j <= len(signatures)-1:
+            sum_of_diffs += len(compare_signatures(signatures[i], signatures[j]))
+            i -= 1
+            j += 1
+        if sum_of_diffs == 0:
+            results[0] = idx+1
+        if sum_of_diffs == 1:
+            results[1] = idx+1
+    return results
 
-    def traverse_pattern(idx_to_hash):
-        '''Go from top to bottom or left to right. If you find neighboring 
-        lines/cols that are the same, expand your search. If you keep finding 
-        identical pairs when you reach the border of the pattern, this is your 
-        range of mirror reflection. Then calculate the score.'''
 
-        i, j = 0, 1
-        while True:
-            if idx_to_hash[i] != idx_to_hash[j]: # if no pair at indexes i and j, advance forward
-                if j - i == 1: # necessary for edge cases in which we have four identical rows/cols
-                    j += 1
-                i = j - 1
-                if not (i >= 0 and j < len(idx_to_hash)): # at border, no reflection tracked
-                    return 0
-            else: # if pair found at i and j, advance back and forward
-                i -= 1
-                j += 1
-                if not (i >= 0 and j < len(idx_to_hash)): # at border, tracking a reflection
-                    return ceil((i+j)/2)
+def check_mirror(data):
+    '''Returns a tuple of lists: (ln, ls), (cn, cs)
+    l: lines
+    c: columns
+    n: no smudges
+    s: with smudges'''
+    line_signatures, col_signatures = get_signatures(data)
+    return find_reflections(line_signatures), find_reflections(col_signatures)
 
-    line_hashes, col_hashes = get_hashes(data)
-    idx_to_hash_line = {idx: hsh for idx, hsh in enumerate(line_hashes)}
-    idx_to_hash_cols = {idx: hsh for idx, hsh in enumerate(col_hashes)}
 
-    line = traverse_pattern(idx_to_hash_line) * 100
-    cols = traverse_pattern(idx_to_hash_cols)
-
-    return line + cols
+def find_symmetries(data):
+    '''Sums the results for part 1 and part 2'''
+    without_smudges, with_smudges = 0, 0
+    for mirror in data:
+        lines, cols = check_mirror(mirror)
+        without_smudges += (100 * lines[0] + cols[0])
+        with_smudges += (100 * lines[1] + cols[1])
+    return without_smudges, with_smudges
 
 
 TEST_DATA = '''#.##..##.
@@ -72,12 +90,14 @@ TEST_DATA = '''#.##..##.
 #....#..#'''
 
 print('Testing...')
-print('Part 1:', sum((find_reflection(data) for data in parse_data(TEST_DATA))) == 405)
+p1, p2 = find_symmetries(parse_data(TEST_DATA))
+print('Part 1:', p1 == 405)
+print('Part 2:', p2 == 400)
+
 
 with open('inp', mode='r', encoding='utf-8') as inp:
     print('Solution...')
     actual_data = inp.read()
-    #print('Part 1:', sum((find_reflection(data) for data in parse_data(actual_data))))
-
-# for part 2 to work - comparing hashes will not work. converting 
-# lines/cols to binary numbers and then bitwise ops, maybe?
+    p1, p2 = find_symmetries(parse_data(actual_data))
+    print('Part 1:', p1)
+    print('Part 2:', p2)
